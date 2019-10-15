aedb974f6430a478d059fda2a76406c5e2834096
Johanes Pao <sleeplessgreencode@gmail.com>
2019-10-15 17:02:46 +0700

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,10 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location!', undefined)
     } else {
+      console.log(body.daily.data[0])
       const temp = body.currently.temperature
       const precipProb = body.currently.precipProbability
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + precipProb + '% chance of rain.')
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + precipProb + '% chance of rain.')
     }
   })
 }
